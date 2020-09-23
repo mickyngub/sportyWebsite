f7bf3953f59bfbe34edc84e510036c909f9e67b1
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-09-23 15:38:11 +0700

    add background picture

--- src/Pages/FindCourt.js
+++ src/Pages/FindCourt.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import Header from "../Components/Header";
+
+import "./FindCourt.css";
+
 const FindCourt = () => {
-  return <div>This is find court page</div>;
+  return (
+    <div>
+      <Header />
+      <div className="body"></div>
+    </div>
+  );
 };
 
 export default FindCourt;
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,16 +2,17 @@ import React from "react";
 import Header from "../Components/Header";
 import MainSport from "../Components/MainSport";
 import Promotion from "../Components/Promotion";
-import Nearby from "../Components/Nearby";
 import Footer from "../Components/Footer";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+
+      <div className="body"></div>
       <MainSport />
       <Promotion />
-      <Nearby />
+      {/* <Nearby /> */}
       <Footer />
     </div>
   );
